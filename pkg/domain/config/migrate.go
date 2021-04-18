a343c31445828055b4cd66d74dd026428e1ab938
Arthur Santana <arthur.santana@sensedia.com>
2021-04-18 12:28:45 -0300

    [TASK-0]-Creation of model income and model for expenses and incomes

--- pkg/domain/config/migrate.go
+++ pkg/domain/config/migrate.go
@@ -3,6 +3,8 @@ package config
 import (
 	"log"
 
+	expenseModel "github.com/Mangaba-Labs/ape-finance-api/pkg/domain/expense/model"
+	incomeModel "github.com/Mangaba-Labs/ape-finance-api/pkg/domain/income/model"
 	stockModel "github.com/Mangaba-Labs/ape-finance-api/pkg/domain/stock/model"
 	userModel "github.com/Mangaba-Labs/ape-finance-api/pkg/domain/user/model"
 	"gorm.io/gorm"
@@ -16,8 +18,7 @@ type Migrate struct {
 // MigrateAll database migration
 func (m *Migrate) MigrateAll() (err error) {
 	log.Println("Migrating database... 🤞")
-	// err = m.DB.AutoMigrate(&user.User{})
-	err = m.DB.AutoMigrate(&userModel.User{}, &stockModel.StockModel{})
+	err = m.DB.AutoMigrate(&userModel.User{}, &stockModel.StockModel{}, &expenseModel.ExpenseModel{}, &expenseModel.ExpenseCategories{}, &incomeModel.IncomeModel{}, &incomeModel.IncomeCategories{})
 
 	if err != nil {
 		log.Fatal("Something went wrong on db migration process...\n ", err)
